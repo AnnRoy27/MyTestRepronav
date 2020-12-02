0d723a94874760523b5d776f30ce670151120bee
AnnRoy27 <annroy2708@gmail.com>
2020-12-02 20:03:09 +0530

    This is my first commit

--- GitTest/src/main/java/page/Test2.java
+++ GitTest/src/main/java/page/Test2.java
@@ -1,11 +1,10 @@
 package page;
 
-public class LoginPage {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test");
-System.out.println("test2");
+
 	}
 
 }
